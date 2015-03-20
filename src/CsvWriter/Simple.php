a7e839eca55b9be4793e2052fc536ac476f8fa93
Christian Bolstad <christian@carnaby.se>
2015-03-20 22:03:19 +0100

    tidy up

--- src/CsvWriter/Simple.php
+++ src/CsvWriter/Simple.php
@@ -35,7 +35,6 @@ class SimpleCsvWrite {
 
 
 	private $fileName;
-	private $fileHandle;
 	private $logFileHeaderSet;
 	private $orderLog;
 	private $folderName = 'data/'; 
@@ -54,15 +53,15 @@ class SimpleCsvWrite {
 	/* From: http://www.php.net/manual/en/function.str-getcsv.php#88773 and http://www.php.net/manual/en/function.str-getcsv.php#91170 */
 	function str_putcsv( $input, $delimiter = ';', $enclosure = '"' ) {
 		// Open a memory "file" for read/write...
-		$fp = fopen( 'php://temp', 'r+' );
+		$filePipe = fopen( 'php://temp', 'r+' );
 		// ... write the $input array to the "file" using fputcsv()...
-		fputcsv( $fp, $input, $delimiter, $enclosure );
+		fputcsv( $filePipe, $input, $delimiter, $enclosure );
 		// ... rewind the "file" so we can read what we just wrote...
-		rewind( $fp );
+		rewind( $filePipe );
 		// ... read the entire line into a variable...
-		$data = fread( $fp, 1048576 );
+		$data = fread( $filePipe, 1048576 );
 		// ... close the "file"...
-		fclose( $fp );
+		fclose( $filePipe );
 		// ... and return the $data to the caller, with the trailing newline from fgets() removed.
 		return rtrim( $data, "\n" );
 	}
