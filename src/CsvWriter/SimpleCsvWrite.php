52db6af2cda7f40bce5fa5cc4a9e1aa45e67e92d
Christian Bolstad <christian@carnaby.se>
2015-03-20 22:06:30 +0100

    composerify

--- src/CsvWriter/SimpleCsvWrite.php
+++ src/CsvWriter/SimpleCsvWrite.php
@@ -1,34 +1,6 @@
 <?php
 
 
-class writeFile {
-  public $lineEnding="\n";
-  private $file;
-  private $fileHandle;
-
-  function __construct($file,$mode='w'){
-    $this->file=$file;
-    if ( !$this->fileHandle = fopen( $this->file, $mode ) ) {
-      throw new Exception("Cannot open file ($this->file)");
-    }
-  }
-
-  function write( $str = '' ) {
-    if ( fwrite( $this->fileHandle, $str ) === FALSE ) {
-        throw new Exception("Cannot write to file ($this->file)");
-    }
-  }
-
-  function writeLine( $str = '' ) {
-    if ( fwrite( $this->fileHandle, $str.$this->lineEnding ) === FALSE ) {
-        throw new Exception("Cannot write to file ($this->file)");
-    }
-  }
-
-    function __destruct() {
-      fclose( $this->fileHandle );
-    }
-  }
 
 
 class SimpleCsvWrite {
